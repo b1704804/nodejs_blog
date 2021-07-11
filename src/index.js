62d1219495f4a2eec2ce753676b4dde1b49d558d
b1704804 <quangdinh647@gmail.com>
2021-07-11 10:15:56 +0700

    Intial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const port = 3000;
+app.use(morgan('combined'))
+
+app.get('/', (req, res) => {
+    return res.send (`
+        <h1 style="color:green;"> Hello World! </h1>
+    `);
+})
+
+app.listen(port, () => console.log('Example app listening at http://localhost:${port}'))
